8d1db75d0b681752cc2dadc5840df3b8d4f6f6ab
Miguel Chateloin <mchateloin@cenergistic.com>
2020-06-09 01:21:35 -0500

    add font awesome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,22 @@ import { Link } from "gatsby"
 import "../components/global.css"
 import Image from "../components/image"
 import SEO from "../components/seo"
-// import FA from "@fortawesome/fontawesome-free"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faCode,
+  faMusic,
+  faPaintBrush,
+  IconDefinition,
+} from "@fortawesome/free-solid-svg-icons"
 
-const MenuItem: React.FC = () => (
-  <div className="bg-gray-200 h-16 w-16 rounded-full"></div>
+interface MenuItemProps {
+  icon: IconDefinition
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ icon = faCode }) => (
+  <div className="bg-gray-200 h-16 w-16 rounded-full flex justify-center">
+    <FontAwesomeIcon className="self-center" icon={icon} />
+  </div>
 )
 
 const IndexPage: React.FC = () => (
@@ -14,11 +26,14 @@ const IndexPage: React.FC = () => (
     <SEO title="Miguel Chateloin" />
     <div className="container mx-auto">
       <h1>Miguel Chateloin</h1>
-      <p>A pretty cool guy with a cool guy tagline that goes here.</p>
+      <p>
+        A pretty cool guy with a cool guy tagline that goes here.{" "}
+        <FontAwesomeIcon icon="code" />
+      </p>
       <div className="flex flex-row justify-between">
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
+        <MenuItem icon={faCode} />
+        <MenuItem icon={faMusic} />
+        <MenuItem icon={faPaintBrush} />
       </div>
     </div>
 
