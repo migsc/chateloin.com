6d0a96c3ec8a77d4122e31d1e84ec36421f4719b
Miguel Chateloin <mchateloin@cenergistic.com>
2020-06-28 16:00:28 -0500

    refactor hard skill section search logic into a method

--- src/pages/code/SkillsSection.tsx
+++ src/pages/code/SkillsSection.tsx
@@ -10,6 +10,67 @@ import {
   getTagMapFromTagNames,
 } from "../../utils"
 
+interface HardSkillSearchResults {
+  tagsFiltered: Tag[]
+  skillsFiltered: HardSkill[]
+}
+
+const getHardSkillSearchResultsFiltered = (
+  skills: HardSkill[],
+  tagsByName: TagMap,
+  searchText: string
+): HardSkillSearchResults => {
+  const nameSearcher = new Fuse(skills, {
+    includeScore: true,
+    keys: ["name"],
+  })
+
+  const activeTagNames = Object.values(tagsByName)
+    .filter(({ active }) => active)
+    .map(({ name }) => name)
+
+  const searchResultsForName = nameSearcher
+    .search(searchText)
+    .map(({ item }) => item)
+
+  const searchResultsForTag = skills.filter(
+    skill =>
+      intersection(skill.tags, activeTagNames).length === activeTagNames.length
+  )
+
+  const skillsFiltered =
+    searchText === "" && activeTagNames.length === 0
+      ? skills
+      : intersectionBy(
+          searchResultsForName.length > 0
+            ? searchResultsForName
+            : searchResultsForTag,
+          searchResultsForTag.length > 0 || activeTagNames.length > 0
+            ? searchResultsForTag
+            : searchResultsForName,
+          "name"
+        )
+
+  const hardSkillTagsByNameFromSearchResults = getTagMapFromTagNames(
+    skillsFiltered.map(({ tags: tagNames }) => tagNames).flat()
+  )
+
+  const tagsFiltered =
+    searchText === "" && activeTagNames.length === 0
+      ? Object.values(tagsByName)
+      : Object.values(
+          mapValues(tagsByName, (t: Tag) => ({
+            ...t,
+            count: hardSkillTagsByNameFromSearchResults[t.name]?.count ?? 0,
+          }))
+        )
+
+  return {
+    skillsFiltered,
+    tagsFiltered,
+  }
+}
+
 const HardSkillSearchResult: React.FC<HardSkill> = ({ name, tags }) => (
   <p className="mb-4" key={name}>
     <span className="font-bold">{name}</span> <br /> {tags.join(", ")}
@@ -42,57 +103,11 @@ const SkillsSection: React.FC<Props> = ({
   onSearchChange,
   onHardSkillTagClick,
 }) => {
-  const nameSearcher = new Fuse(skills.hard, {
-    includeScore: true,
-    keys: ["name"],
-  })
-
-  const activeTagNames = Object.values(hardSkillTagsByName)
-    .filter(({ active }) => active)
-    .map(({ name }) => name)
-
-  const searchResultsForName = nameSearcher
-    .search(searchText)
-    .map(({ item }) => item)
-
-  const searchResultsForTag = skills.hard.filter(
-    skill =>
-      intersection(skill.tags, activeTagNames).length === activeTagNames.length
+  const hardSkillSearchResults = getHardSkillSearchResultsFiltered(
+    skills.hard,
+    hardSkillTagsByName,
+    searchText
   )
-
-  const hardSkillsFiltered =
-    searchText === "" && activeTagNames.length === 0
-      ? skills.hard
-      : intersectionBy(
-          searchResultsForName.length > 0
-            ? searchResultsForName
-            : searchResultsForTag,
-          searchResultsForTag.length > 0 || activeTagNames.length > 0
-            ? searchResultsForTag
-            : searchResultsForName,
-          "name"
-        )
-
-  const hardSkillTagsByNameFromSearchResults = getTagMapFromTagNames(
-    hardSkillsFiltered.map(({ tags: tagNames }) => tagNames).flat()
-  )
-
-  const hardSkillTagsFiltered =
-    searchText === "" && activeTagNames.length === 0
-      ? Object.values(hardSkillTagsByName)
-      : Object.values(
-          mapValues(hardSkillTagsByName, (t: Tag) => ({
-            ...t,
-            count: hardSkillTagsByNameFromSearchResults[t.name]?.count ?? 0,
-          }))
-        )
-
-  console.log("activeTagNames", activeTagNames)
-  console.log("hardSkillTagsByName", hardSkillTagsByName)
-  console.log("searchText", searchText)
-  console.log("searchResultsForName", searchResultsForName)
-  console.log("searchResultsForTag", searchResultsForTag)
-  console.log("hardSkillsFiltered", hardSkillsFiltered)
 
   return (
     <div className="mt-16 mb-16">
@@ -110,12 +125,12 @@ const SkillsSection: React.FC<Props> = ({
           </label>
         </div>
         <div>
-          {hardSkillTagsFiltered.map((tag: Tag) => (
+          {hardSkillSearchResults.tagsFiltered.map((tag: Tag) => (
             <TagPill {...tag} onClick={onHardSkillTagClick} />
           ))}
         </div>
         <div>
-          {hardSkillsFiltered.map(({ name, tags }) => (
+          {hardSkillSearchResults.skillsFiltered.map(({ name, tags }) => (
             <HardSkillSearchResult name={name} tags={tags} />
           ))}
         </div>
