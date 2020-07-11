872070a82c463fa317f48fd13cb06e6c4ab6c9a4
Miguel Chateloin <mchateloin@cenergistic.com>
2020-07-11 16:24:55 -0500

    under construction notice

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Helmet from "react-helmet"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useStaticQuery, graphql } from "gatsby"
 import {
   faCode,
   faMusic,
@@ -11,25 +11,60 @@ import Layout from "../../components/Layout"
 import NavButton from "./NavButton"
 import styles from "./HomePage.module.css"
 
+interface HomePageState {
+  underConstruction: boolean
+}
+
+interface HomePageActions {}
+
+const useContainer = (): [HomePageState, HomePageActions] => {
+  const {
+    site: { underConstruction },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          underConstruction
+        }
+      }
+    `
+  )
+
+  const actions = {}
+
+  return [{ underConstruction }, actions]
+}
+
+const header = ["miguel", <br />, "chateloin"]
+const headerUnderConstruction = ["under", <br />, "construction"]
+
+const tagline = `A pretty cool guy with a cool guy tagline that goes here. 
+Maybe it can go up to here? This is a nice length.`
+const taglineUnderConstruction = `I'm done messing around with WordPress 
+templates. I'm taking some time right now to build something from scratch using 
+the tools I love. Check back soon!`
+
 const HomePage: React.FC = () => {
+  const [{ underConstruction }] = useContainer()
+  console.log("underConstruction", underConstruction)
   return (
     <Layout bodyClassName={styles.background} scrollable={false}>
       <SEO title="Miguel Chateloin" />
       <div className="max-w-xs py-12 md:py-48 md:max-w-sm lg:max-w-lg sm:mx-auto">
         <h1 className="text-4xl mb-4">
-          miguel <br />
-          chateloin
+          {underConstruction ? headerUnderConstruction : header}
         </h1>
         <p className="text-xl">
-          A pretty cool guy with a cool guy tagline that goes here. Maybe it can
-          go up to here? This is a nice length.
+          {underConstruction ? taglineUnderConstruction : tagline}
           <FontAwesomeIcon icon="code" />
         </p>
-        <div className="flex flex-row justify-around mt-32 mx-auto">
-          <NavButton icon={faCode} linkTo="code" />
-          <NavButton icon={faMusic} />
-          <NavButton icon={faPaintBrush} />
-        </div>
+        {!underConstruction && (
+          <div className="flex flex-row justify-around mt-32 mx-auto">
+            <NavButton icon={faCode} linkTo="code" />
+            <NavButton icon={faMusic} />
+            <NavButton icon={faPaintBrush} />
+          </div>
+        )}
       </div>
 
       {/* <Link to="/page-2/">Go to page 2</Link> */}
