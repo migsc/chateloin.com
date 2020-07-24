3aaabc9ecca88042fa56ec47b148fab1a38b6435
Miguel Chateloin <mchateloin@cenergistic.com>
2020-07-24 16:37:57 -0500

    a more dynamic test for inputOf

--- src/components/code/ExperienceSection/__tests__/Timeline.tsx
+++ src/components/code/ExperienceSection/__tests__/Timeline.tsx
@@ -6,14 +6,15 @@ import moment from "moment"
 describe("Timeline indexOf", () => {
   it("indexOf works", () => {
     const mStartOfThisMonth = moment().startOf("month")
-    const baseDate = mStartOfThisMonth.format("YYYY-MM-DD")
-    for (let i = 0; i < 150; i++) {
+    const baseDate = mStartOfThisMonth.format("YYYY-MM")
+    const nMonthsToTest = mStartOfThisMonth.diff(moment("2008-01-01"), "month")
+
+    for (let i = 0; i <= nMonthsToTest; i++) {
       const offsetDate = mStartOfThisMonth
         .clone()
         .subtract(i, "month")
-        .format("YYYY-MM-DD")
-      const index = indexOf(baseDate, offsetDate)
-      expect(index).toBe(i)
+        .format("YYYY-MM")
+      expect(indexOf(baseDate, offsetDate)).toBe(i)
     }
   })
 })
